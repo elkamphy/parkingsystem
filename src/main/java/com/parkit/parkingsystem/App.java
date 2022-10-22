6630cdf2d4ba395ca8a5506321fa14c442ab6731
EL KAMPHY <elkamphy@steves-MacBook-Pro.local>
2022-10-22 12:09:02 +0100

    initial commit

--- src/main/java/com/parkit/parkingsystem/App.java
+++ src/main/java/com/parkit/parkingsystem/App.java
@@ -4,6 +4,11 @@ import com.parkit.parkingsystem.service.InteractiveShell;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+/**
+ * 
+ * @author elkamphy
+ *
+ */
 public class App {
     private static final Logger logger = LogManager.getLogger("App");
     public static void main(String args[]){
